e439ceb26facb2bfb9a677beb2cfe2e6c21b155f
chiarafais <chiara0915@gmail.com>
2024-03-29 23:32:11 +0100

    ultime modifiche

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ const changeColor = function () {
     document.querySelector("header .navBar button").className = "";
   }
 };
+
+const emme = function () {
+  let m = document.querySelectorAll("svg g g");
+
+  let mFiltrate = [];
+  for (let i = 0; i < m.length; i++) {
+    if (m[i].style.display === "block") {
+      mFiltrate.push(m[i]);
+    }
+  }
+  let i = 0;
+
+  setInterval(() => {
+    let numeroRandom = Math.floor(Math.random() * mFiltrate.length - 1) + 1;
+    if (i < 40) {
+      i++;
+      mFiltrate[numeroRandom].style.display = "none";
+    } else {
+      for (let i = 0; i < mFiltrate.length; i++) {
+        mFiltrate[i].style.display = "block";
+      }
+      i = 0;
+    }
+  }, 100);
+};
+
+emme();
